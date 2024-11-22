bcfc335ca599369aa8b8c8b0763dd854aba64e93
bisVivek <vivek5832017@gmail.com>
2024-11-22 13:00:10 +0530

    car as super class and vehicle as sub class

---
+++ src/car.java
@@ -0,0 +1,7 @@
+public class car {
+    protected String brand="BMW"; // attribute
+
+    public void honk(){
+        System.out.println("Teet teet");
+    }
+}
---
+++ src/vehicle.java
@@ -0,0 +1,9 @@
+public class vehicle extends car{
+
+    private String modelNo="Ford";
+
+    public static void main(String[]args){
+        vehicle myvehicle=new vehicle();
+
+    }
+}
